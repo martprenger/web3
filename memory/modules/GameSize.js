45c04d124315188cb7b033e503e160904567ec19
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-05-28 20:26:54 +0200

    added game size thrue a class

---
+++ memory/modules/GameSize.js
@@ -0,0 +1,39 @@
+
+
+export class GameSize {
+    defaultGameSize = 36;
+    constructor() {
+        this.createSizeOptions();
+    }
+
+    createSizeOptions() {
+        const sizes = [
+            {text: 'Small', value: 16},
+            {text: 'Medium', value: 36},
+            {text: 'Biggy', value: 64}
+        ];
+
+        const dropdown = document.getElementById("gameSize");
+        const gameSize = this.getGameSize();
+
+        sizes.forEach(size => {
+            let option = document.createElement("option");
+            option.text = size.text;
+            option.value = size.value;
+
+            if (size.value === gameSize) {
+                option.selected = true;
+            }
+
+            dropdown.appendChild(option);
+        });
+
+        dropdown.addEventListener('change', () => {
+            localStorage.setItem('gameSize', dropdown.value);
+        });
+    }
+
+    getGameSize() {
+        return Number(localStorage.getItem('gameSize')) || this.defaultGameSize;
+    }
+}
