73469218a87bcbd7600b83920652dfaaf0bc88a1
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-06-19 20:58:25 +0200

    registratie and login in php

--- memory/modules/GameType.js
+++ memory/modules/GameType.js
@@ -1,3 +1,4 @@
+
 export class GameType {
     defaultGameType = 'alphabet';
     constructor() {
@@ -79,5 +80,6 @@ export class GameType {
             console.error('Error fetching flag data:', error);
             return [];
         }
+
     }
 }
